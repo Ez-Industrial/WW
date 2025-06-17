41d8cfdf8b5bcf96bae2ae906f6e2bf800388bb5
Ez-Industrial <jesus.escobedo@ez-industrial.com>
2025-06-17 09:14:54 -0500

    agregando paginas

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,44 @@
+function Inicio (){
+      <Router>
+      <Routes>
+        <Route path="/inicio" element={<Home />} />
+      </Routes>
+        <h1>Autenticación con Firebase</h1>
+    
+          {usuario ? (
+        <div>
+          <p>Bienvenido, {usuario.email}!</p>
+          <button onClick={() => { logout(); setUsuario(null); }}>Cerrar sesión</button>
+        </div>
+      ) : (
+        <div>
+          <input type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button onClick={async () => setUsuario(await registrarUsuario(email, password))}>Registrarse</button>
+          <button onClick={async () => setUsuario(await loginCorreo(email, password))}>Iniciar sesión</button>
+        </div>
+      )}
+      
+       {/* Formulario para enviar datos al backend */}
+      <div>
+        <h2>Enviar datos al backend</h2>
+        <input
+          type="text"
+          placeholder="Escribe un mensaje"
+          value={mensaje}
+          onChange={(e) => setMensaje(e.target.value)}
+        />
+        <button onClick={enviarDatos}>Enviar</button>
+      </div>
+
+      <h2>Historial de mensajes</h2>
+      <ul>
+        {Array.isArray(datos) && datos.length > 0 ? (
+          datos.map((item, index) => <li key={index}>{item.mensaje}</li>)
+        ) : (
+          <p>No hay mensajes aún.</p>
+        )}
+      </ul>
+    </Router>
+}
+export default Inicio;
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,6 @@
+function Sobre() {
+  return <h1>Esta es la página Sobre</h1>;
+}
+
+export default Sobre;
+
