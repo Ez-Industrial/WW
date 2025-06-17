7add90433aad75a80c147b534c2469952d18314e
Ez-Industrial <jesus.escobedo@ez-industrial.com>
2025-06-17 12:21:24 -0500

    sfa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Puedes cambiarlo a otro puerto si es necesario
-    host: 'localhost', // Asegura que se sirva en localhost
+    port: 5173,
+    host: 'localhost',
   },
+  build: {
+    rollupOptions: {
+      external: ['firebase/auth']
+    }
+  }
 });
