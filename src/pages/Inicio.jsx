fca60518a6ead599127be2f5940e271b38c114c5
Ez-Industrial <jesus.escobedo@ez-industrial.com>
2025-07-15 09:58:33 -0500

    act lap

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -33,7 +33,7 @@ function Inicio (){
   return (
     <div className="contenedor-centro">
       <h1>Wash Wheels</h1>
-      <p>Ingresa tus datos para continuar.</p>
+      <p>Ingresa tus datos para continuar. lap</p>
       <div className= "datos">
       <button onClick={lavarauto}>Solicitar Lavar Auto</button>
       <button onClick={lavador}>Ser Lavador</button>
